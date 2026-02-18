75a8ed7c3a5340e803d653907f02ae2d23733e02
ElNavas-8 <nicolas.navas@alumnos.uneatlantico.es>
2026-02-18 17:29:54 +0100

    feat: sum on printf

--- Char.c
+++ Char.c
@@ -1,8 +1,10 @@
 #include <stdio.h>
 
 int main(void)
-{
+{   
+    int var1 = -1, var2 = 2; 
     char var[]= "Hola Mundo";
+    printf("var1 = %i, var2 = %u var1+var2 = %i\n", var1, var2, var1+var2);
     printf("%s\n", var);
     return 0;
 }
