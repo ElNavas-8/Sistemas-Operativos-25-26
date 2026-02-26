39a0399a6f246c04f22deffe5e7f21abe23daffd
ElNavas-8 <nicolas.navas@alumnos.uneatlantico.es>
2026-02-26 15:55:59 +0100

    feat: Join strings and get length

---
+++ JoinChar.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include <string.h>
+
+int main (void){
+
+    char str1[12] = "Pene ";
+    char str2[12] = "Sucio";
+    char str3[12];
+
+    strcpy(str3, str1);
+    printf("strcpy(str1, str2) = %s\n", str3);
+    
+    int length = strlen(str1);
+    printf("strlen(str1) = %d\n", length);
+
+    strcat(str1, str2);
+    printf("strcat(str1, str2) = %s\n", str1);
+}
